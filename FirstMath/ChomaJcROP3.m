e0c184186485cc8377715940b41e6f2e808690a7
HenryDrake <93620645+HenryDrake@users.noreply.github.com>
2021-12-07 12:30:55 +0100

    Add files via upload
    Kontrolní úkol č. 3 – MATLAB

---
+++ FirstMath/ChomaJcROP3.m
@@ -0,0 +1,27 @@
+
+
+    function [A,B] = ChomaJcROP3(D,F)
+    M File pro cROP Choma Jakub;
+% ChomaJcROP3
+%   Matice v Matlab
+writematrix(D,matrixD.txt)
+writematrix(F,matrixF.txt)
+read(D,1)
+read(F,2)
+
+A = min(X3);
+B = max(X3);
+input(Zadejte,"D")
+input(Zadejte,"F")
+or
+Read Matrix from Spreadsheet File
+Import numeric data from matrix.xls into a matrix.
+
+
+"Vítejte v programu Matice. Výsledkem je ' A, B' .";
+ save
+
+ pause
+ run
+ close
+end
